b3ae12ff95db347134a5f57789616ced7b078502
urmovosa <urmo.vosa@gmail.com>
2016-09-13 15:01:52 +0200

    Updated paths

--- Fix_SNP_names_alleles.sh
+++ Fix_SNP_names_alleles.sh
@@ -7,9 +7,18 @@ module load Java
 # -p P-value threshold (use 1)
 # LogAnnotations.txt is input for script script_SNP_enrollment_QC.R
 
+# hg18 (Lifted over)
 java -Xmx55g -Xms55g -jar /groups/umcg-wijmenga/tmp04/umcg-uvosa/FixSnpNamesGWAS-1.0.4-SNAPSHOT-jar-with-dependencies.jar \
--r /groups/umcg-wijmenga/tmp02/projects/umcg-uvosa/temp_annotation_files/GiantStatistics_Filtered_on_Maf0.001_Indels_AmbigiousSnps.vars.gz \
+-r /groups/umcg-wijmenga/tmp04/umcg-uvosa/temp_annotation_files/GiantStatistics_Filtered_on_Maf0.001_Indels_AmbigiousSnps.vars.gz \
 -a /groups/umcg-wijmenga/tmp04/umcg-uvosa/input_Marc_Jan/rs_synonyms/SNP_synonyms.txt.gz \
--o /groups/umcg-wijmenga/tmp04/umcg-uvosa/input_Marc_Jan/output_files/ \
--s /groups/umcg-wijmenga/tmp04/umcg-uvosa/curated_GWAS_full_summary_statistics/hg19_rs/ \
--p 1 2>&1 | tee /groups/umcg-wijmenga/tmp04/umcg-uvosa/input_Marc_Jan/Log_Annotations.txt
+-o /groups/umcg-wijmenga/tmp04/umcg-uvosa/curated_GWAS_full_summary_statistics/hg_to_QC/ \
+-s /groups/umcg-wijmenga/tmp04/umcg-uvosa/curated_GWAS_full_summary_statistics/filtered_SS_files/hg18_hg19/ \
+-p 1 2>&1 | tee /groups/umcg-wijmenga/tmp04/umcg-uvosa/curated_GWAS_full_summary_statistics/filtered_SS_files/log_files/fix_SNPName_hg18.txt
+
+# hg19
+java -Xmx55g -Xms55g -jar /groups/umcg-wijmenga/tmp04/umcg-uvosa/FixSnpNamesGWAS-1.0.4-SNAPSHOT-jar-with-dependencies.jar \
+-r /groups/umcg-wijmenga/tmp04/umcg-uvosa/temp_annotation_files/GiantStatistics_Filtered_on_Maf0.001_Indels_AmbigiousSnps.vars.gz \
+-a /groups/umcg-wijmenga/tmp04/umcg-uvosa/input_Marc_Jan/rs_synonyms/SNP_synonyms.txt.gz \
+-o /groups/umcg-wijmenga/tmp04/umcg-uvosa/curated_GWAS_full_summary_statistics/hg_to_QC/ \
+-s /groups/umcg-wijmenga/tmp04/umcg-uvosa/curated_GWAS_full_summary_statistics/filtered_SS_files/hg19/ \
+-p 1 2>&1 | tee /groups/umcg-wijmenga/tmp04/umcg-uvosa/curated_GWAS_full_summary_statistics/filtered_SS_files/log_files/fix_SNPName_hg19.txt
